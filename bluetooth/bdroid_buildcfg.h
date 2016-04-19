4e676d8366d56927e8c688d78bf9afac8e9234e9
Zeeshan <rqmok@hotmail.com>
2016-04-19 14:20:23 +0930

    bluetooth: Update according to BCM4329 capabilities.
    Change-Id: I1e06f266f484953ebf61128294a83daa063c5a81

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,8 +1,11 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME "HTC Incredible S"
+
+#define BTM_DEF_LOCAL_NAME "Incredible S"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLUETOOTH_QCOM_SW TRUE
-#define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
-#define BTC_INCLUDED TRUE
+
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
+
 #endif
